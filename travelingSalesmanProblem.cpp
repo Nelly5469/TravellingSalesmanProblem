d48027fc9634777a00267d2a1bba28b5cc837b2a
Nelly5469 <nelly.delgado5469@alumnos.udg.mx>
2025-09-15 19:20:29 -0600

    fix: establish parent node in closedList

--- travelingSalesmanProblem.cpp
+++ travelingSalesmanProblem.cpp
@@ -1,3 +1,4 @@
+// TRAVELLING SALESMAN PROBLEM
 #include <iostream>
 #include <stack>
 #include <vector>
@@ -5,65 +6,83 @@
 
 using namespace std;
 
-struct Node {
-    string name; // Nombre de la ciudad
-    vector<Node*> childrenNodes; // Ciudades donde hay viajes directos desde la ciudad actual
-    vector<Node*> parentNodes; // Ciudades donde hay viajes directos hacia la ciudad actual
+class Node
+{
+public:
+    string name;                  // Nombre de la ciudad
+    vector<Node *> childrenNodes; // Ciudades donde hay viajes directos desde la ciudad actual
+    Node *parentNode;
 
     Node(string cityName) : name(cityName) {} // Constructor para inicializar el nombre de la ciudad
 };
 
-class Graph {
+class Graph
+{
 public:
-    vector<Node*> nodes; // Almacena los nodos del grafo
+    vector<Node *> nodes; // Almacena los nodos del grafo
 
-    void addNode(const string& cityName) {
+    void addNode(const string &cityName)
+    {
         nodes.push_back(new Node(cityName)); // Agrega un nuevo nodo al grafo
     }
 
-    void addEdge(const string& fromCity, const string& toCity) {
-        Node* fromNode = findNode(fromCity);
-        Node* toNode = findNode(toCity);
-        if (fromNode && toNode) {
-            fromNode->childrenNodes.push_back(toNode); // Establece la conexión entre las ciudades
-            toNode->parentNodes.push_back(fromNode);
+    void addEdge(const string &fromCity, const string &toCity)
+    {
+        Node *fromNode = findNode(fromCity);
+        Node *toNode = findNode(toCity);
+        if (fromNode && toNode)
+        {
+            fromNode->childrenNodes.push_back(toNode); // Establece la conexiï¿½n entre las ciudades
+            // toNode->parentNodes.push_back(fromNode);
         }
     }
 
-    Node* findNode(const string& cityName) {
-        for (Node* node : nodes) {
-            if (node->name == cityName) {
+    Node *findNode(const string &cityName)
+    {
+        for (Node *node : nodes)
+        {
+            if (node->name == cityName)
+            {
                 return node; // Devuelve el nodo si se encuentra
             }
         }
         return nullptr; // Devuelve nullptr si no se encuentra
     }
 
-    void displayGraph() {
-        for (Node* node : nodes) {
+    void displayGraph()
+    {
+        for (Node *node : nodes)
+        {
             cout << "Ciudad: " << node->name << " -> ";
-            for (Node* child : node->childrenNodes) {
+            for (Node *child : node->childrenNodes)
+            {
                 cout << child->name << " ";
             }
             cout << endl;
         }
     }
 
-    ~Graph() {
-        for (Node* node : nodes) {
+    ~Graph()
+    {
+        for (Node *node : nodes)
+        {
             delete node; // Libera la memoria de los nodos
         }
     }
 };
 
-int main() {
+int main()
+{
     Graph graph;
 
     // Inicializa las ciudades
     const vector<string> CITIES = {"NY", "CHI", "DEN", "HOU", "URB", "LA", "TOR", "CALG"};
 
+    vector<vector<Node *>> solutions;
+
     // Agrega nodos al grafo
-    for (const string& city : CITIES) {
+    for (const string &city : CITIES)
+    {
         graph.addNode(city);
     }
 
@@ -83,30 +102,69 @@ int main() {
     // Muestra el grafo
     graph.displayGraph();
 
-    const Node* INIT_STATE = graph.findNode("NY");
-    const Node* FINAL_STATE = graph.findNode("LA");
-    //1. Lista abierta contiene el edo. inicial
-    stack<Node*> openList.push(INIT_STATE);
-    //2. Lista cerrada vacia
-    stack<Node*> closedList;
+    Node *INIT_STATE = graph.findNode("NY");
+    Node *FINAL_STATE = graph.findNode("LA");
+    if (INIT_STATE == nullptr || FINAL_STATE == nullptr)
+    {
+        cout << "Error: uno de los nodos no se encontrÃ³." << endl;
+        return -1; // O maneja el error de otra manera
+    }
+    // 1. Lista abierta contiene el edo. inicial
+    stack<Node *> openList;
+    openList.push(INIT_STATE);
+    // 2. Lista cerrada vacia
+    stack<Node *> closedList;
 
-    Node* currentNode;
+    Node *currentNode;
+    Node *auxNode, *auxNode2;
     unsigned int i;
+    vector<Node *> currentSolution;
 
-    //3. Lista abierta vacia?
-    while(!openList.empty()){
-        //3.1 No
-        currentNode = openList.top(); //3.1.1 Nodo actual en lista abierta
-        //3.1.2 Nodo actual en lista cerrada (extraer de lista cerrada)
+    // 3. Lista abierta vacia?
+    while (!openList.empty())
+    {
+        // 3.1 No
+        currentNode = openList.top(); // 3.1.1 Nodo actual en lista abierta
+        // 3.1.2 Nodo actual en lista cerrada (extraer de lista cerrada)
         closedList.push(currentNode);
         openList.pop();
-        if(closedList.top() != FINAL_STATE){ //3.1.3
-            //3.1.3.1
-            for(Node *children : currenNode.childrenNodes){ //3.1.3.1.1
-                openList.push(children);//3.1.3.1.2
-                ////3.1.3.1.3 Establecer apuntadores desde descendientes incorporado hacia padre en lista cerrada (creo que ya se hace)
+        if (closedList.top() != FINAL_STATE)
+        { // 3.1.3
+            // 3.1.3.1
+            for (Node *children : currentNode->childrenNodes)
+            {                            // 3.1.3.1.1
+                openList.push(children); // 3.1.3.1.2
+                ////3.1.3.1.3 Establecer apuntadores desde descendientes incorporado hacia padre en lista cerrada
+                children->parentNode = currentNode;
             }
         }
+        else
+        {
+            auxNode = closedList.top();
+            //while (auxNode != INIT_STATE)
+            while (auxNode != INIT_STATE && auxNode != nullptr)
+            {
+                currentSolution.push_back(auxNode);
+                auxNode2 = auxNode->parentNode;
+                //auxNode->parentNode = nullptr; // quitar ref a padre //deberia agregar una bandera a los padres ya visitados?
+                auxNode = auxNode2;
+            }
+            solutions.push_back(currentSolution);
+            currentSolution.clear();
+        }
+    }
+
+    // mostrar soluciones
+    cout << endl
+         << endl
+         << "Trayectorias de soluciones" << endl;
+    for (vector <Node *> solution : solutions)
+    {
+        for (Node *city : solution)
+        {
+            cout << city->name << " ";
+        }
+        cout << endl;
     }
 
     return 0;
